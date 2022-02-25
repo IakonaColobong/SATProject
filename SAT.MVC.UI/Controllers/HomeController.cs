fabdfac098d713a4f1f6e8cb8d181eaea4597b43
jlester99 <janet.lester1@outlook.com>
2022-02-25 13:35:53 -0600

    Created Contact Form

--- SAT.MVC.UI/Controllers/HomeController.cs
+++ SAT.MVC.UI/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using SAT.UI.MVC.Models;
+using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
 
 namespace SAT.MVC.UI.Controllers
 {
@@ -11,7 +16,6 @@ namespace SAT.MVC.UI.Controllers
         }
 
         [HttpGet]
-        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your app description page.";
@@ -26,5 +30,55 @@ namespace SAT.MVC.UI.Controllers
 
             return View();
         }
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                //send the cvm back to the form with completed inputs
+                return View(cvm);
+            }
+            //build the message
+            string message = $"An email has been received from {cvm.Name} with a subject of {cvm.Subject}. Please respond to {cvm.Email} with your response to the following message: <br/>{cvm.Message}";
+            //MailMessage - What sends the email
+            MailMessage mm = new MailMessage(
+            //FROM
+            ConfigurationManager.AppSettings["EmailUser"].ToString(),
+            //TO
+            ConfigurationManager.AppSettings["EmailTo"].ToString(),
+            cvm.Subject,
+            message);
+
+            //MailMessage properties
+            //Allow HTML in the email
+            mm.IsBodyHtml = true;
+            mm.Priority = MailPriority.High;
+            //Respond to the sender, and not our website
+            mm.ReplyToList.Add(cvm.Email);
+
+            //SmtpClient - This is the info from the host that allows this to be sent
+            SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings["EmailClient"].ToString());
+
+            //Client Credentials
+            client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["EmailUser"].ToString(), ConfigurationManager.AppSettings["EmailPass"].ToString());
+
+            //Try to send the email
+            try
+            {
+                //attempt to send
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.CustomerMessage = $"We're sorry your request could not be completed at this time. Please try again later. Error Message:<br/>{ex.StackTrace}";
+                return View(cvm);
+            }
+
+            return View("EmailConfirmation", cvm);
+        }
+
+
+
+
     }
 }
